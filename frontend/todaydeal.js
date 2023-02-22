decb1b32197f86c1633798fdc89b554de72c30e7
faiyaz7070 <malickfaiyaz415@gmail.com>
2023-02-22 22:40:20 +0530

    day2

---
+++ frontend/todaydeal.js
@@ -0,0 +1,100 @@
+export  const todaydeal=[
+    {
+img:"https://m.media-amazon.com/images/I/41kg-+XWoxL._AC_SY200_.jpg",
+discount:26,
+DealofDay:"Deal of the Day",
+desc:"Samsung Galaxy M04"
+    },
+    {
+        img:"https://m.media-amazon.com/images/I/41Ml+bxR8UL._AC_SY200_.jpg",
+discount:32,
+DealofDay:"Deal of the Day",
+desc:"Best offers on vivo smart phones"
+
+    },
+    {
+        img:"https://m.media-amazon.com/images/I/41skB7kHQbL._AC_SY200_.jpg",
+        discount:39,
+        DealofDay:"Deal of the Day",
+        desc:"High Performance Desktop Computer"
+            },
+            {
+                img:"https://m.media-amazon.com/images/I/71JdO22IWBL._CB613517346_._SY200__AC_SY200_.png",
+        discount:72,
+        DealofDay:"Deal of the Day",
+        desc:"Home improvement essential"
+        
+            },
+            {
+              img:"https://m.media-amazon.com/images/I/21Zm6-TFZEL._AC_SY200_.jpg",
+                discount:69,
+                DealofDay:"Deal of the Day",
+                desc:"Upto 50% off on Storage Device"
+                    },
+    {
+img:"https://m.media-amazon.com/images/I/41KGGnBmpgL._AC_SY200_.jpg",
+discount:19,
+ DealofDay:"Deal of the Day",
+ desc:"Tecno Pop 7 Pro starting Rs 6799"
+                
+ },
+                    {
+                        img:"https://m.media-amazon.com/images/I/51kdwtY1KvL._AC_SY200_.jpg",
+                        discount:87,
+                        DealofDay:"Deal of the Day",
+                        desc:"Wallpaper and wall art"
+                            },
+                            {
+                                img:"https://m.media-amazon.com/images/I/31Jsbjg-WfL._AC_SY200_.jpg",
+                        discount:70,
+                        DealofDay:"Deal of the Day",
+                        desc:"Blaupunkt Bestseller Speaker"
+                        // next
+                            },
+                            {
+                                img:"https://m.media-amazon.com/images/I/31iM0EfRZ-L._AC_SY200_.jpg",
+                        discount:56,
+                        DealofDay:"Deal of the Day",
+                        desc:"kitchen Products Amazon Brands and.."
+                        
+                            },
+                            {
+                                img:"https://m.media-amazon.com/images/I/41uGjvXbeBL._AC_SY200_.jpg",
+                                discount:94,
+                                DealofDay:"Deal of the Day",
+                                desc:"Best offers on PC and Electronic acces.."
+                                    },
+                                    {
+                                        img:"https://m.media-amazon.com/images/I/61u2JcRfLWL._SX679__AC_SY200_.jpg",
+                                discount:67,
+                                DealofDay:"Deal of the Day",
+                                desc:"offer on it"
+                                
+                                    },
+                                    {
+                                      img:"https://m.media-amazon.com/images/I/41J2lcX2TkS._AC_SY200_.jpg",
+                                        discount:61,
+                                        DealofDay:"Deal of the Day",
+                                        desc:"Best of Smart bulbs"
+                                            },
+                            {
+                        img:"https://m.media-amazon.com/images/I/51K8wC5PksL._AC_SY200_.jpg",
+                        discount:57,
+                         DealofDay:"Deal of the Day",
+                         desc:"Dried Fruits Nuts and olive oil"
+                                        
+                         },
+                                            {
+                                                img:"https://m.media-amazon.com/images/I/31XFxTn1DCL._AC_SY200_.jpg",
+                                                discount:75,
+                                                DealofDay:"Deal of the Day",
+                                                desc:"Top deals on computer acces"
+                                                    },
+                                                    {
+                                                        img:"https://m.media-amazon.com/images/I/31YahxzrgWL._AC_SY200_.jpg",
+                                                discount:46,
+                                                DealofDay:"Deal of the Day",
+                                                desc:"Best offers from V-Guard products"
+                                                
+                                                    },
+]
